80c0645c72317446a921a8f9eb72cd765f45980f
Nik Eastburn <nik@ndeast.com>
2017-07-28 10:47:11 -0400

    add some comments

--- bitgrabber.py
+++ bitgrabber.py
@@ -1,26 +1,26 @@
+
 #!usr/local/bin/python3
+
+# Created 7/28/17 by ndeast
 
 import requests
 import time
-import threading
+
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 fileID = "PLX OB"
 filedate = datetime.now().strftime("%Y-%m-%d %H.%M")
 filename = (fileID + " " + str(filedate) + ".txt")
+btcStringList = []
+
+# Settings ########
 collections = 50
 sleepTime = 5
 amountToWrite = 3
+###################
 
-btcStringList = []
-
-def collector():
-    r = requests.get("https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=50")
-    soup = BeautifulSoup(r.content, "html.parser")
-    btcString = soup.text + ""
-    btcStringList.append(btcString)
-
+# Main function collects and writes data based on settings. 
 def main():
     for i in range(collections):
         print("list length is: " + str(len(btcStringList)))
@@ -30,6 +30,14 @@ def main():
             writeToFile()
         time.sleep(sleepTime)
 
+# Retrieves api request, parses html to string, and appends string to list
+def collector():
+    r = requests.get("https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=50")
+    soup = BeautifulSoup(r.content, "html.parser")
+    btcString = soup.text + ""
+    btcStringList.append(btcString)
+
+# Opens text file for appending, and writes each string in the btcStringList onto a new line in the file.
 def writeToFile():
     print("writing to file")
     with open(filename, 'a') as f:
